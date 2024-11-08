fe83c4c224bdad712fdab0572c5508a2e0d1cd68
kissmark <naterivas12@gmail.com>
2024-11-08 17:04:51 -0500

    Primer commit

---
+++ 03-adminpro/src/app/app-routing.module.ts
@@ -0,0 +1,25 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+//Modulos
+import { PagesRoutingModule } from './pages/pages.routing';
+
+import { NopagefoundComponent } from './nopagefound/nopagefound.component';
+import { AuthRoutingModule } from './auth/aut.routing';
+
+const routes: Routes = [
+
+{path: '', redirectTo: '/dashboard',pathMatch:'full'},
+{path: '**',component: NopagefoundComponent}
+
+];
+
+@NgModule({
+  declarations:[],
+  imports: [
+    RouterModule.forRoot(routes),
+    PagesRoutingModule,
+    AuthRoutingModule
+  ],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ 03-adminpro/src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { AuthModule } from './auth/auth.module';
+
+import { NopagefoundComponent } from './nopagefound/nopagefound.component';
+
+import { PagesModule } from './pages/pages.module';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NopagefoundComponent,
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    PagesModule,
+    AuthModule,
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
